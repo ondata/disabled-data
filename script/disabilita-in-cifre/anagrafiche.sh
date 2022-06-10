5e567e6d5d5d936d17ec2d5afbe9d7da4cc86bae
Andrea Borruso <aborruso@gmail.com>
2022-06-10 19:47:07 +0200

    crea anagrafica humar readable

---
+++ script/disabilita-in-cifre/anagrafiche.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -x
+set -e
+set -u
+set -o pipefail
+
+folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+mkdir -p "$folder"/../data
+mkdir -p "$folder"/tmp
+
+# crea markdown dei file
+
+ mlrgo --c2m put '$file="[".$file.".xls](rawdata/".$file.".xls)"' "$folder"/../../data/disabilita-in-cifre/processing/anagrafica.csv >"$folder"/../../data/disabilita-in-cifre/anagrafica.md
