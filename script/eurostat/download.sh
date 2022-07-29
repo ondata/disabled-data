76c27db8d1b368f5bd4700f794105f54926532f1
Andrea Borruso <aborruso@gmail.com>
2022-07-29 18:57:41 +0200

    pulizia

--- script/eurostat/download.sh
+++ script/eurostat/download.sh
@@ -8,8 +8,6 @@ set -o pipefail
 folder="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 mkdir -p "$folder"/../../data/eurostat/raw
-#touch "$folder"/../../data/eurostat/raw/keep.txt
-
 
 while read -r line; do
   wget -O "$folder"/../../data/eurostat/raw/"$line".tsv.gz "https://ec.europa.eu/eurostat/estat-navtree-portlet-prod/BulkDownloadListing?sort=1&downfile=data%2F$line.tsv.gz"
